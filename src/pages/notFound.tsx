8552913780ac848ed01c48e0f05af0a51efd1eb1
Le Cong Thang <lecongthang@Les-MacBook-Pro.local>
2021-06-04 18:35:40 +0700

    [1]Feature/technical-foundation: Create app infrastructure, added static assets

---
+++ src/pages/notFound.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const NotFound: FC = () => (
+  <h1 className="text-center">Opps!!! Not found</h1>
+);
+
+export default NotFound;
---
+++ src/services/repository.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const baseURL = 'https://dummyapi.io/data/api/';
+const headers = {
+  'app-id': '60349db146ff8b0837d18351'
+};
+
+const instance = axios.create({
+  baseURL,
+  headers
+});
+instance.interceptors.response.use(
+  (response) => response.data,
+  (error) => Promise.reject(error)
+);
+export default instance;
