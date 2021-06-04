f8fb3f2c6a7bc0855dd74425c4daee7d303c6cfb
Le Cong Thang <lecongthang@Les-MacBook-Pro.local>
2021-06-04 15:58:49 +0700

    Init project with typescript

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export const ALERT_TYPES = {
+  SUCCESS: 'success',
+  ERROR: 'error',
+  INFO: 'info',
+  WARNING: 'warning'
+};
+
+export const ALERT_TIME = 3; // Seconds
---
+++ src/constants/types.ts
@@ -0,0 +1,4 @@
+export type FnType = (param?: object) => void;
+export interface AnyObject {
+  [key: string]: any;
+}
