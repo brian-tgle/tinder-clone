b496b0ecb8978db957f819cce1a324ae3ff53e8a
Le Cong Thang <lecongthang@Les-MacBook-Pro.local>
2021-06-05 11:52:50 +0700

    [2]Feature/profile-card-ui: Implement profile card UI (Picture and actions)

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,26 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import ProfileCard from 'components/profileCard';
+import UserRepository from 'services/userRepository';
+import { DEFAULT_API_PARAMS } from 'constant';
 
-const Home: FC = () => (
-  <h1>This is home page</h1>
-);
+const Home: FC = () => {
+  const { LIMIT, PAGE } = DEFAULT_API_PARAMS;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    UserRepository.getAll(LIMIT, PAGE).then((data) => {
+      console.log(data);
+      setUsers(data.data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }, []);
+
+  return (
+    <div className="tinder-show">
+      <ProfileCard {...users[0]} />
+    </div>
+  );
+};
+
 
 export default Home;
